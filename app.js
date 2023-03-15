76d6b408976a408ade49983a9d0b9c3311865e2b
2086-1Webmaster <79549344+2086-1Webmaster@users.noreply.github.com>
2023-03-15 11:03:17 -0400

    Halfway done through video 3
    Started a new concept and am working through it.

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 const app = Vue.createApp({
     data() {
         return {
+            url: 'http://www.thenetninja.co.uk',
             showBooks: true, 
-            title: 'The Final Empire',
-            author: 'Brandon Sanderson',
-            age: 45,
-            x: 0,
-            y: 0
+            books: [
+                { title: 'name of the wind', author: 'patrick rothfuss', img: 'assets/1.jpg' },
+                { title: 'the way of kings', author: 'brandon sanderson', img: 'assets/2.jpg' },
+                { title: 'the final empire', author: 'juju maholms', img: 'assets/3.jpg' },
+            ]
 
         }
     },
